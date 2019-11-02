d78bdcee0cc2e9bc6ab06fc02d43da82fa1152a9
Eddie Brazier (RIT Student) <ecb9413@ad.rit.edu>
2019-11-01 22:48:22 -0400

    started working on traps

---
+++ Puppeteering/Assets/Scripts/BoulderSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*RGDC Day of the Dead Jam
+ *Puppeteering
+ *Eddie Brazier
+ */
+
+public class BoulderSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //activates when the player enters the spawn collider
+    private void OnTriggerEnter(Collider other)
+    {
+        //TODO: cause boulder to spawn when player enters using raycasting
+    }
+}
---
+++ Puppeteering/Assets/Scripts/Trap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*RGDC Day of the Dead Jam
+ *Puppeteering
+ *Eddie Brazier
+ */
+
+public class Trap : MonoBehaviour
+{
+    //enum for type of trap to spawn from trap activation
+    public enum TrapType
+    {
+        Boulder,
+        Pitfall
+    }
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Activates when the player enters the object's trigger
+    private void OnTriggerEnter(Collider other)
+    {
+
+    }
+}
