5ebb410724f37b84afe83619d87d98cc71c2d967
Eddie Brazier <ecb9413@ad.rit.edu>
2019-11-02 19:06:55 -0400

    Main Menu is complete except for options

---
+++ Puppeteering/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*RGDC Day of the Dead Jam
+ *Puppeteering
+ *Eddie Brazier
+ */
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //load the next scene in the build
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+
+        Application.Quit();
+    }
+}
