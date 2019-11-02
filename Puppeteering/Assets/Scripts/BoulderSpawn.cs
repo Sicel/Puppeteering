62398da436a9cfac1d1417f9c453aedc86284896
Eddie Brazier <ecb9413@ad.rit.edu>
2019-11-02 10:44:24 -0400

    working on boulder spawning

--- Puppeteering/Assets/Scripts/BoulderSpawn.cs
+++ Puppeteering/Assets/Scripts/BoulderSpawn.cs
@@ -9,6 +9,14 @@ using UnityEngine;
 
 public class BoulderSpawn : MonoBehaviour
 {
+    //field for the boulder prefab this trap will spawn when triggered
+    [SerializeField]
+    private GameObject boulder;
+
+    //field for boulder force
+    [SerializeField]
+    private float boulderForce;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +32,36 @@ public class BoulderSpawn : MonoBehaviour
     //activates when the player enters the spawn collider
     private void OnTriggerEnter(Collider other)
     {
-        //TODO: cause boulder to spawn when player enters using raycasting
+        //disable trigger to prevent multiple trap triggers
+        gameObject.GetComponent<BoxCollider>().enabled = false;
+
+        //send out a raycast to a wall
+        Physics.Raycast(gameObject.transform.position, transform.TransformDirection(Vector3.forward), out RaycastHit hit, Mathf.Infinity);
+
+        //check if the distance of the raycast hit is great enough
+        if(hit.distance > 3f)
+        {
+            //get a vector from the distance between the point of raycast hit and the trap point
+            Vector3 spawnVector = gameObject.transform.position - hit.point;
+
+            //normalize spawnVector for boulder force calculation
+            Vector3 boulderVector = spawnVector.normalized;
+
+            //spawn boulder
+            GameObject newBoulder = Instantiate(boulder, hit.point, Quaternion.identity);
+
+            //move boulder forward to prevent clipping
+            Vector3 adjustVector = boulderVector;
+            adjustVector *= 0.5f;
+            newBoulder.transform.position += adjustVector;
+
+            //add force to the boulder
+            newBoulder.GetComponent<Rigidbody>().AddForce(boulderVector * boulderForce);
+
+            Debug.Log("GO BOULDER GO");
+
+            //delete this trap object
+            Destroy(gameObject);
+        }
     }
 }
