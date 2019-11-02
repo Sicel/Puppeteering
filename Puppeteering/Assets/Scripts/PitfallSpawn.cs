3d5cc240906dfcfe34da88a3d4410ff715d9662b
Eddie Brazier <ecb9413@ad.rit.edu>
2019-11-02 11:18:17 -0400

    pitfall trap version 1 complete

---
+++ Puppeteering/Assets/Scripts/PitfallSpawn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*RGDC Day of the Dead Jam
+ *Puppeteering
+ *Eddie Brazier
+ */
+
+public class PitfallSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //activates when another object collides with this one
+    private void OnCollisionEnter(Collision collision)
+    {
+        //check if the collision is occuring with the player
+        if(collision.gameObject.tag == "Player")
+        {
+            //destroy this gameobject
+            Destroy(gameObject);
+        }
+    }
+}
