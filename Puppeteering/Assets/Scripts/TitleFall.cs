ee1c2007bc92128ad1399acd278122491cfa6706
Eddie Brazier <ecb9413@ad.rit.edu>
2019-11-02 18:12:16 -0400

    FAST FALL

---
+++ Puppeteering/Assets/Scripts/TitleFall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*RGDC Day of the Dead Jam
+ *Puppeteering
+ *Eddie Brazier
+ */
+
+public class TitleFall : MonoBehaviour
+{
+    //field for downward force
+    [SerializeField]
+    private float dropForce;
+
+    //field for the puppet's rigidbody
+    Rigidbody puppetBody;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+       puppetBody = gameObject.GetComponent<Rigidbody>();
+
+       puppetBody.AddForce(Vector3.down * dropForce, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
