d0153c37011795404e3399bb042a080540d8111c
Iain Davis (RIT Student) <icd1636@ad.rit.edu>
2019-11-02 21:54:29 -0400

    Line Drawing

---
+++ Puppeteering/Assets/Scripts/FingerLineDrawer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerLineDrawer : MonoBehaviour
+{
+    private SpringJoint springJoint;
+    private LineRenderer lineRenderer;
+    private Transform connectedBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+        springJoint = gameObject.GetComponent<SpringJoint>();
+        connectedBody = springJoint.connectedBody.GetComponentInParent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lineRenderer.SetPosition(0, springJoint.anchor + gameObject.transform.position);
+        lineRenderer.SetPosition(1, springJoint.connectedAnchor + connectedBody.position);
+    }
+}
